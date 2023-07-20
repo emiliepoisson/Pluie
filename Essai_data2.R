4dd1e1cd7ce913b7fd6227c903efe2018b5db218
aziz25allouche <139364833+aziz25allouche@users.noreply.github.com>
2023-07-20 16:33:38 +0200

    Train_Spectral.R

--- Essai_data2.R
+++ Essai_data2.R
@@ -2,17 +2,6 @@ library("RclusTool")
 library("DTWUMI")
 Clust_F1=list()
 d=read.csv("Douala/CML_Douala/L1_172.22.204.1P4.1-172.22.204.6P4.1_29A-B.csv")
-for (i in 1:6) {
-  data=d[6576*(i-1)+1:6576*i,c(6,7,8,9,10,11)]
-  dataimp=DTWUMI_imputation(data,gap_size_threshold=100)
-  data2=dataimp$output
-  SIMZP <- computeGaussianSimilarityZP(data2)
-  SIM2=(SIMZP^alpha)*(Q^(1-alpha))
-  RES2= spectralClusteringNg(SIM2,K=12)
-  plot(data$TSL_mean[6576*(i-1)+1:6576*i],col=RES2$label,ylim=c(0,8),pch=RES2$label)
-  lines(d$Rgage[6576*(i-1)+1:6576*i],col="green")
-  Clust_F1=c(Clust_F1,list(RES2$label))
-}
 
 trapeze <- function(x1,x2,taille){
   q <- matrix(ncol=taille,nrow =taille)
@@ -32,6 +21,19 @@ trapeze <- function(x1,x2,taille){
 Q=trapeze(1000,1000,6576)
 
 alpha=0.4
+
+for (i in 1:6) {
+  data=d[6576*(i-1)+1:6576*i,c(6,7,8,9,10,11)]
+  dataimp=DTWUMI_imputation(data,gap_size_threshold=100)
+  data2=dataimp$output
+  SIMZP <- computeGaussianSimilarityZP(data2)
+  SIM2=(SIMZP^alpha)*(Q^(1-alpha))
+  RES2= spectralClusteringNg(SIM2,K=12)
+  plot(data$TSL_mean[6576*(i-1)+1:6576*i],col=RES2$label,ylim=c(0,8),pch=RES2$label)
+  lines(d$Rgage[6576*(i-1)+1:6576*i],col="green")
+  Clust_F1=c(Clust_F1,list(RES2$label))
+}
+
 
 
 
